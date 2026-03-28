7f550eed2d386aeabbfe116df1bb0f80aa2a4a52
Nikan <nikankad@protonmail.com>
2026-03-28 18:41:42 -0400

    refactor: update collate function in train_qn.py and add slurm script for cutout training

--- model/train_qn_cutout.py
+++ model/train_qn_cutout.py
@@ -21,6 +21,7 @@ from helpers import (
     collate_fn_test,
     get_dataset_lengths,
     log_epoch,
+    collate_fn_cutout,
 )
 from qnmodel import QuartzNetBxR
 from model_spec import write_training_config
@@ -153,7 +154,7 @@ def _build_dataloaders(train_ds, val_ds, test_ds, batch_size, num_workers, rank,
     train_loader = DataLoader(
         train_ds,
         batch_sampler=train_sampler,
-        collate_fn=collate_fn,
+        collate_fn=collate_fn_cutout,
         **_loader_kwargs(num_workers),
     )
 
